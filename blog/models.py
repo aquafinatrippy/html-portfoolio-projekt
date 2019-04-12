27990b98206ab7e397c2f2a78fc5c5dcc4258707
Tanel Tekko <45185224+aquafinatrippy@users.noreply.github.com>
2019-04-13 00:30:16 +0300

    Lisatud bootstrap ja appe paremaks tehtud

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Blog():
-    
+class Blog(models.Model):
+    pealkiri = models.CharField(max_length=255)
+    kuupaev = models.DateTimeField()
+    sisu = models.TextField(max_length=255)
+    pilt = models.ImageField(upload_to='images/')
