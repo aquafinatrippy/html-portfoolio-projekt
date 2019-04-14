29d5cdd4158f1129000061f155d8e7d94bc88dee
Tanel Tekko <45185224+aquafinatrippy@users.noreply.github.com>
2019-04-14 19:16:27 +0300

    ready2go

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,4 @@ from .models import Blog
 def allblogs(request):
     blogs = Blog.objects
     return render(request, 'blog/allblogs.html', {'blogs': blogs})
+
--- jobs/models.py
+++ jobs/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    too = models.CharField(max_length=255, default="LINK")
 
 
